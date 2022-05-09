69ade63c33b5eba245ef6b8013c17f6f2d53fc2c
Vadym Boyarchuk <vadimca1.boyarchuc@gmail.com>
2022-05-09 19:01:46 +0300

    init frontend

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      HELLLOOO BRAZA
+      HELLOO BRAZA
     </div>
   );
 }
