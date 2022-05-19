2ecddc7ddfffaa44a4e6fbebc46be70dcf7af75d
Vadym Boyarchuk <vadimca1.boyarchuc@gmail.com>
2022-05-19 18:56:00 +0300

    add query for product

--- frontend/src/pages/Mbarara/Mbarara.tsx
+++ frontend/src/pages/Mbarara/Mbarara.tsx
@@ -1,27 +1,13 @@
 import NavBar from "../../modules/Mbarara/components/Navbar";
-import Product from "../../modules/Mbarara/components/ProductList/Product/Product";
-import BoltIcon from '@mui/icons-material/Bolt';
+import FlashDealsList from "../../modules/Mbarara/components/ProductList/FlashDealsList/FlashDealsList";
+
 
 const Mbarara = () => {
-  return <div>
-    <NavBar />
-    <div style={{ margin: '15px' }}>
-      <div>
-        <div style={{ marginBottom: '5px' }}>
-          <div style={{ display: 'flex', flexDirection: 'row', }}>
-            <div style={{ color: '#D23F57' }}>
-              <BoltIcon />
-            </div>
-            <div style={{ fontWeight: 500, fontSize: '20px' }}>
-              Flash Deals
-            </div>
-          </div>
-        </div>
-        <div>
-          <Product />
-        </div>
-      </div>
+  return (
+    <div>
+      <NavBar />
+      <FlashDealsList />
     </div>
-  </div>
+  );
 };
 export default Mbarara;
