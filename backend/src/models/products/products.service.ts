6afdb949ed1af29cb16906d6e255c7f55ebc454e
Vadym Boyarchuk <vadimca1.boyarchuc@gmail.com>
2022-05-21 14:39:06 +0300

    add pagination

--- backend/src/models/products/products.service.ts
+++ backend/src/models/products/products.service.ts
@@ -5,6 +5,22 @@ import { Injectable } from '@nestjs/common';
 import { Product } from './entities/product.entity';
 import { Op } from 'sequelize';
 
+interface paginateArgs {
+  page: number;
+  pageSize: number;
+}
+
+const paginate = (query, { page, pageSize }: paginateArgs) => {
+  const offset = page;
+  const limit = pageSize;
+
+  return {
+    ...query,
+    offset,
+    limit,
+  };
+};
+
 @Injectable()
 export class ProductsService {
   constructor(@InjectModel(Product) private productRepository: typeof Product) { }
@@ -17,14 +33,16 @@ export class ProductsService {
     return await this.productRepository.findAll();
   }
 
-  async getFlashDealsProducts(): Promise<Product[]> {
-    return await this.productRepository.findAll({
+  async getFlashDealsProducts(page: number, pageSize: number): Promise<Product[]> {
+    return await this.productRepository.findAll(paginate({
       where: {
         discount: {
           [Op.ne]: null
         }
-      }
-    })
+      },
+    },
+      { page, pageSize },
+    ));
   }
 
   async getProductById(id: number): Promise<Product> {
